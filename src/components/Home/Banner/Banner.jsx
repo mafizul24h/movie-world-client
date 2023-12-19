4375b392896aa94d3fd6372cb94031537d957476
mafizul24h <mafizul24h@gmail.com>
2023-12-19 18:07:54 +0600

    Banner Update

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
-
-import banner from './../../../assets/banner/jawaan-banner.png'
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
 
 const Banner = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/movies')
+            .then(res => res.json())
+            .then(data => setMovies(data))
+    }, [])
+
     return (
-        <div className="banner-img h-[100vh] px-16 grid grid-cols-1 md:grid-cols-2 gap-8 justify-center items-center pb-8">
+        <div className="banner-img h-[100vh] px-16 grid grid-cols-1 md:grid-cols-2 gap-8 justify-center items-center pb-16">
             <div className='pt-16'>
                 <h1 className='text-4xl md:text-6xl font-bold text-sky-600'>BEST WAY OF ENTERTAINMENT</h1>
                 <p className='text-2xl font=bold text-gray-100 font-bold mt-6'>MOVIES AS YOU DEMAND AT USD</p>
                 <p className='text-yellow-600 font-bold text-2xl'>10/MONTH</p>
             </div>
-            <div className='h-5/6 md:h-1/2 text-center '>
-                <img className='h-3/4 md:h-full md:w-1/2 mx-auto relative' src="https://goldandhra.com/wp-content/uploads/2023/08/New-Poster-Revealed-From-Shah-Rukh-Khans-Jawan-Movie.webp" alt="" />
-                <button className='button-primary absolute -ml-20 -mt-6'>Watch Now</button>
+            <div className='h-5/6 md:h-1/2 text-center'>
+                <>
+                    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                        {movies.map(singleMovie => <SwiperSlide>
+                            {/* <img className='h-3/4 rounded-md md:h-96 md:w-1/2 mx-auto ' src={singleMovie.movie} alt={singleMovie.name} /> */}
+                            <iframe width="100%" height="480" src={singleMovie.movie} title={singleMovie.name} frameborder="0" allow="accelerometer; autoplay" allowfullscreen></iframe>
+                        </SwiperSlide>)}
+                    </Swiper>
+                </>
+                <button className='button-primary absolute -ml-20 -mt-6 z-20'>Watch Now</button>
             </div>
         </div>
     );
