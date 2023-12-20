587f4d3bf1e2445b179215ff82b60bfd3a92a298
mafizul24h <mafizul24h@gmail.com>
2023-12-20 23:36:05 +0600

    Update

--- src/components/Home/EnjoyMovie/EnjoyMovie.jsx
+++ src/components/Home/EnjoyMovie/EnjoyMovie.jsx
@@ -6,9 +6,10 @@ const EnjoyMovie = () => {
     return (
         <div className='bg-black bg-opacity-40 p-4 md:py-16 md:px-24'>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 justify-center items-center max-w-7xl mx-auto'>
-                <div >
+                <div className='space-y-2'>
                     <h1 className='text-4xl font-bold text-gray-100 uppercase mb-3'>Enjoy It <span className='text-orange-500'>Movies</span></h1>
                     <p className='text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae corrupti amet omnis animi dolorem ullam dignissimos alias libero nobis neque.</p>
+                    <button className='button-primary'>Watch Now</button>
                 </div>
                 <div className='col-span-2 relative'>
                     <img className='' src={image} alt="Image" />
